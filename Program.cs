19cb8d4c30b829a86c0fd28ed73cd3bc7cd75c34
Temmee228 <ugrymovas.20@edu.ystu.ru>
2023-01-10 12:38:07 +0300

    New commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+double a;
+double b;
+double total;
+String oper;
+
+oper = "";
+Console.WriteLine("Возможные операции: +, -, *, /");
+
+
+     Console.WriteLine("Введите a:");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите оператор (+, -, *, /, ^, log):");
+oper = Console.ReadLine();
+
+Console.WriteLine("Введите b:");
+b = Convert.ToDouble(Console.ReadLine());
+
+switch (oper)
+{
+    case "+":
+        total = a + b;
+        Console.WriteLine(a + " + " + b + " = " + total);
+        Console.ReadKey(true);
+        break;
+
+    case "-":
+        total = a - b;
+        Console.WriteLine(a + " - " + b + " = " + total);
+        Console.ReadKey(true);
+        break;
+
+    case "*":
+        total = a * b;
+        Console.WriteLine(a + " * " + b + " = " + total);
+        Console.ReadKey(true);
+        break;
+
+    case "/":
+        total = a / b;
+        Console.WriteLine(a + " / " + b + " = " + total);
+        Console.ReadKey(true);
+        break;
+}
